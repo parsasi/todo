b289aaa35b78de23e3362fe6b7e0363aefe1c2b1
parsasi <parsasi@rocketmail.com>
2020-10-28 18:56:27 +0330

    build added;

---
+++ build/precache-manifest.2ea18465e955f716b904cefb84fa6092.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b132af72f672c260dd13a058113ca27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9cd9b9db5aedd6a147b6",
+    "url": "/static/css/main.002ad189.chunk.css"
+  },
+  {
+    "revision": "6762a4f11fa13e33903f",
+    "url": "/static/js/2.a23bc0bb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a23bc0bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cd9b9db5aedd6a147b6",
+    "url": "/static/js/main.6ce31f26.chunk.js"
+  },
+  {
+    "revision": "4fb080de24627b03831e",
+    "url": "/static/js/runtime-main.f16ea130.js"
+  }
+]);
